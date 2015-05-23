2fc5cb1ad1568e5ce519724d3d96c57a7d7c24f3
Mikael Jenkler <mikael@jenkler.com>
2015-05-23 13:08:42 +0200

    Added postlink template

--- library.js
+++ library.js
@@ -2,13 +2,38 @@
 	"use strict";
 	var postlink = {};
 
+	
+	postlink.addAdminNavigation = function(header, callback) {
+	header.plugins.push({
+	route: '/plugins/postlink',
+	icon: 'fa-link',
+	name: 'Postlink'
+	});
+	callback(null, header);
+	};
+	
+	function renderAdmin(req, res, next)
+	{
+		res.render('admin/postlink', {});
+	}
+
+	postlink.init = function(params, callback)
+	{
+		params.router.get('/admin/plugins/postlink', params.middleware.admin.buildHeader, renderAdmin);
+		params.router.get('/api/admin/plugins/postlink', renderAdmin);
+		callback();
+	};
+
+	
+	
+	
 	postlink.parse = function(data, callback)
 	{
 		if (!data || !data.postData || !data.postData.content)
 		{
 			return callback(null, data);
 		}
-		
+
 		var proxy_url = 'https://www.google.com/?q=';
 		var regex = /href="([^\'\"]+)/g;
 
@@ -16,9 +41,10 @@
 		{
 			data.postData.content = data.postData.content.replace(regex, function(a, b)
 			{
-				return proxy_url+encodeURIComponent(b);
+				return 'href="'+proxy_url+encodeURIComponent(b);
 			});
 		}
+//		console.log(data.postData);
 
 		callback(null, data);
 	};
