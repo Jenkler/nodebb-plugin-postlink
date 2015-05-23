9045d1e285f4c8886d238b3709ddb26743b9a417
mketo <mketo@users.noreply.github.com>
2015-05-23 11:48:52 +0200

    Update library.js
    Added functionality to append proxy urls for urls in postdata.

--- library.js
+++ library.js
@@ -1,6 +1,27 @@
 (function(module) {
 	"use strict";
-	
+	var postlink = {};
+
+	postlink.parse = function(data, callback)
+	{
+		if (!data || !data.postData || !data.postData.content)
+		{
+			return callback(null, data);
+		}
+		
+		var proxy_url = 'https://www.google.com/?q=';
+		var regex = /href="([^\'\"]+)/g;
+
+		if(data.postData.content.match(regex))
+		{
+			data.postData.content = data.postData.content.replace(regex, function(a, b)
+			{
+				return proxy_url+encodeURIComponent(b);
+			});
+		}
+
+		callback(null, data);
+	};
 
 	module.exports = postlink;
 }(module));
