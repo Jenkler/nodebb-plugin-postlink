32abe1016091e97b4fc1fa112ddbdbbd75b5a182
Mikael Jenkler <mikael@jenkler.com>
2016-02-16 20:26:05 +0100

    Inbound and broken urls is now skipped Added target="_blank" for outbound urls

--- library.js
+++ library.js
@@ -1,6 +1,8 @@
 "use strict";
 
 var meta = module.parent.require('./meta');
+var nconf = require.main.require('nconf');
+var url = require('url');
 
 (function(module)
 {
@@ -41,11 +43,12 @@ var meta = module.parent.require('./meta');
 		{
 			data.postData.content = data.postData.content.replace(regex, function(a, b)
 			{
+				if(b.indexOf(url.parse(nconf.get('url')).hostname) !== -1) return 'href="'+b;
 				var pattern = /(\.gif|\.jpg|\.jpeg|\.png|\.svg|youtube\.com)/i;
 				if(pattern.test(b)) return 'href="'+b;
 				var pattern = /^((http|https):\/\/)/;
-				if(!pattern.test(b)) b = "http://" + b;
-				return 'href="'+url_prefix+encodeURIComponent(b);
+				if(!pattern.test(b)) return 'href="'+b; // b = "http://" + b;
+				return 'href="'+url_prefix+encodeURIComponent(b)+'" target="_blank';
 			});
 		}
 		callback(null, data);
