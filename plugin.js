8bc71d4c7304258d30448a321263767135d5d4d2
Mikael Jenkler <mikael@jenkler.com>
2018-08-12 23:03:45 +0200

    Updated compatibility for NodeBB 1.10.x

--- plugin.js
+++ plugin.js
@@ -1,16 +1,14 @@
 "use strict";
 
-var meta = module.parent.require('./meta');
-var nconf = require.main.require('nconf');
-var url = require('url');
+const meta = module.parent.require('./meta');
+const nconf = require.main.require('nconf');
+const url = require('url');
 
-function renderAdmin(req, res, next)
-{
+function renderAdmin(req, res, next) {
 	res.render('admin/postlink', {});
 }
 
-exports.filter_admin_header_build = function(header, callback)
-{
+exports.filterAdminHeaderBuild = function(header, callback) {
 	header.plugins.push({
 		route: '/postlink',
 		icon: 'fa-link',
@@ -19,18 +17,14 @@ exports.filter_admin_header_build = function(header, callback)
 	callback(null, header);
 };
 
-exports.filter_parse_post = function(data, callback)
-{
-	if (!data || !data.postData || !data.postData.content || !meta.config['postlink:urlprefix'])
-	{
+exports.filterParsePost = function(data, callback) {
+	if (!data || !data.postData || !data.postData.content || !meta.config['postlink:urlprefix']) {
 		return callback(null, data);
 	}
 	var regex = /href="([^\'\"]+)/g;
 	var url_prefix = meta.config['postlink:urlprefix'];
-	if(data.postData.content.match(regex))
-	{
-		data.postData.content = data.postData.content.replace(regex, function(a, b)
-		{
+	if(data.postData.content.match(regex)) {
+		data.postData.content = data.postData.content.replace(regex, function(a, b) {
 			if(b.indexOf(url.parse(nconf.get('url')).hostname) !== -1) return 'href="'+b;
 			var pattern = /(\.gif|\.jpg|\.jpeg|\.png|\.svg|youtube\.com)/i;
 			if(pattern.test(b)) return 'href="'+b;
@@ -42,9 +36,9 @@ exports.filter_parse_post = function(data, callback)
 	callback(null, data);
 };
 
-exports.static_app_load = function(data, callback)
-{
+exports.staticAppLoad = function(data, callback) {
 	console.log('Loading Jenkler Postlink plugin');
+
 	data.router.get('/admin/postlink', data.middleware.admin.buildHeader, renderAdmin);
 	data.router.get('/api/admin/postlink', renderAdmin);
 	callback();
