ea15c98e0183a1d7e362accbe3459e26672329bd
Mikael Jenkler <mikael@jenkler.com>
2018-08-20 21:13:59 +0200

    Added runtime version information Replaced tabs with 2 spaces

--- plugin.js
+++ plugin.js
@@ -5,41 +5,38 @@ const nconf = require.main.require('nconf');
 const url = require('url');
 
 function renderAdmin(req, res, next) {
-	res.render('admin/postlink', {});
+  res.render('admin/postlink', {});
 }
 
 exports.filterAdminHeaderBuild = function(header, callback) {
-	header.plugins.push({
-		route: '/postlink',
-		icon: 'fa-link',
-		name: 'Postlink'
-	});
-	callback(null, header);
+  header.plugins.push({
+    route: '/postlink',
+    icon: 'fa-link',
+    name: 'Postlink'
+  });
+  callback(null, header);
 };
-
 exports.filterParsePost = function(data, callback) {
-	if (!data || !data.postData || !data.postData.content || !meta.config['postlink:urlprefix']) {
-		return callback(null, data);
-	}
-	var regex = /href="([^\'\"]+)/g;
-	var url_prefix = meta.config['postlink:urlprefix'];
-	if(data.postData.content.match(regex)) {
-		data.postData.content = data.postData.content.replace(regex, function(a, b) {
-			if(b.indexOf(url.parse(nconf.get('url')).hostname) !== -1) return 'href="'+b;
-			var pattern = /(\.gif|\.jpg|\.jpeg|\.png|\.svg|youtube\.com)/i;
-			if(pattern.test(b)) return 'href="'+b;
-			var pattern = /^((http|https):\/\/)/;
-			if(!pattern.test(b)) return 'href="'+b; // b = "http://" + b;
-			return 'href="'+url_prefix+encodeURIComponent(b)+'" target="_blank';
-		});
-	}
-	callback(null, data);
+  if (!data || !data.postData || !data.postData.content || !meta.config['postlink:urlprefix']) {
+    return callback(null, data);
+  }
+  var regex = /href="([^\'\"]+)/g;
+  var url_prefix = meta.config['postlink:urlprefix'];
+  if(data.postData.content.match(regex)) {
+    data.postData.content = data.postData.content.replace(regex, function(a, b) {
+      if(b.indexOf(url.parse(nconf.get('url')).hostname) !== -1) return 'href="'+b;
+      var pattern = /(\.gif|\.jpg|\.jpeg|\.png|\.svg|youtube\.com)/i;
+      if(pattern.test(b)) return 'href="'+b;
+      var pattern = /^((http|https):\/\/)/;
+      if(!pattern.test(b)) return 'href="'+b; // b = "http://" + b;
+      return 'href="'+url_prefix+encodeURIComponent(b)+'" target="_blank';
+    });
+  }
+  callback(null, data);
 };
-
 exports.staticAppLoad = function(data, callback) {
-	console.log('Loading Jenkler Postlink plugin');
-
-	data.router.get('/admin/postlink', data.middleware.admin.buildHeader, renderAdmin);
-	data.router.get('/api/admin/postlink', renderAdmin);
-	callback();
+  console.log('Loading Jenkler Postlink plugin' + require('./package.json').version);
+  data.router.get('/admin/postlink', data.middleware.admin.buildHeader, renderAdmin);
+  data.router.get('/api/admin/postlink', renderAdmin);
+  callback();
 };
--- templates/admin/postlink.tpl
+++ templates/admin/postlink.tpl
@@ -1,16 +1,16 @@
 <h1>Postlink</h1>
 <form>
-	<p>If you need to add a prefix to outbound links in the posts</p><br />
-	<p>
-		<label for="URL prefix">URL prefix</label>
-		<input type="text" data-field="postlink:urlprefix" title="URL prefix" class="form-control" placeholder="URL prefix"><br />
-	</p>
+  <p>If you need to add a prefix to outbound links in the posts</p><br />
+  <p>
+    <label for="URL prefix">URL prefix</label>
+    <input type="text" data-field="postlink:urlprefix" title="URL prefix" class="form-control" placeholder="URL prefix"><br />
+  </p>
 </form>
 
 <button class="btn btn-lg btn-primary" id="save">Save</button>
 
 <script>
-	require(['admin/settings'], function(Settings) {
-		Settings.prepare();
-	});
+  require(['admin/settings'], function(Settings) {
+    Settings.prepare();
+  });
 </script>
