b924eb220f6627e883055305df4bf6e712bdc336
Mikael Jenkler <mikael@jenkler.com>
2019-07-06 13:22:47 +0200

    Internal link fix

--- plugin.js
+++ plugin.js
@@ -24,11 +24,12 @@ exports.filterParsePost = function(data, callback) {
   var url_prefix = meta.config['postlink:urlprefix'];
   if(data.postData.content.match(regex)) {
     data.postData.content = data.postData.content.replace(regex, function(a, b) {
+      if(b.indexOf('/') === 0) return 'href="'+b;
+      var pattern = /^((http|https):\/\/)/;
+      if(!pattern.test(b)) b='http://'+b 
       if(b.indexOf(url.parse(nconf.get('url')).hostname) !== -1) return 'href="'+b;
       var pattern = /(\.gif|\.jpg|\.jpeg|\.png|\.svg|youtube\.com)/i;
       if(pattern.test(b)) return 'href="'+b;
-      var pattern = /^((http|https):\/\/)/;
-      if(!pattern.test(b)) return 'href="'+b; // b = "http://" + b;
       return 'href="'+url_prefix+encodeURIComponent(b)+'" target="_blank';
     });
   }
